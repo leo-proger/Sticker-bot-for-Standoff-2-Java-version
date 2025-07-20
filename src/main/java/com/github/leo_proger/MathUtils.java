1888c4a91511f203ef792c2f504d0aca44b921bd
Leo Proger <leoproger11@gmail.com>
2025-07-20 20:11:54 +0300

    Немного оптимизированы мат. вычисления

--- src/main/java/com/github/leo_proger/MathUtils.java
+++ src/main/java/com/github/leo_proger/MathUtils.java
@@ -8,12 +8,11 @@ public class MathUtils {
 	}
 
 	public static int getStickerPosByY(int y1, int lotHeight, double yMultiplier, int lotNumber, int lotIndent) {
-		return Math.toIntExact(Math.round(
-				y1 + lotHeight * yMultiplier + lotHeight * lotNumber + lotIndent * lotNumber));
+		return Math.toIntExact(Math.round(y1 + lotHeight * (yMultiplier + lotNumber) + lotIndent * lotNumber));
 	}
 
 	public static int getBuyButtonByY(int y1, int lotHeight, int lotNumber, int lotIndent) {
-		return Math.toIntExact(Math.round(y1 + (double) lotHeight / 2 + lotHeight * lotNumber + lotIndent * lotNumber));
+		return Math.toIntExact(Math.round(y1 + lotHeight * (0.5 + lotNumber) + lotIndent * lotNumber));
 	}
 
 	public static int getStickerWidthAndHeight(int lotHeight, double stickerWidthAndHeightMultiplier) {
